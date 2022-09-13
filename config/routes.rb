4636a9457dc355aba0da82a7d3a0b5b8811730bb
Theophileaseh <theophilenwachan@gmail.com>
2022-09-13 11:09:13 +0100

    create api endpoints and add static view to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'messages#index'
+  resources :messages
+
+  namespace :api do
+    namespace :v1 do
+      resource :messages
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
