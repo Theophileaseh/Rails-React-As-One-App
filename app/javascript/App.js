ee2e3c2b2e5c531bf9517abef5faa90fad60f5aa
Theophileaseh <theophilenwachan@gmail.com>
2022-09-19 12:24:05 +0100

    Fix linter errors

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Greetings from './Components/Greetings';
+import Greetings from './Components/Greetings.js';
 
 const App = () => (
   <Router>
